3232954e2f480111f1d196f29727495e5443db81
Dan Lubbers <lubbers.dan@gmail.com>
2019-07-12 13:54:14 -0400

    Step 10: Sourcing content from the file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@ module.exports = {
     },
     plugins: [
       `gatsby-plugin-sass`,
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            name: 'src',
+            path: `${__dirname}/src/`
+        }
+      }
     ]
 }
