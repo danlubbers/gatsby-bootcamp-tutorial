d20d06e6b94fb3d1c39220a661009b0dc3c82810
Dan Lubbers <lubbers.dan@gmail.com>
2019-07-13 13:37:14 -0400

    Step 17: Rendering Contentful Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,42 +4,76 @@ import Layout from '../Components/Layout';
 import blogStyles from '../pages/blog.module.scss';
 
 const BlogPage = () => {
+    // Pulling in query from Contentful API
     const data = useStaticQuery(graphql`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost ( sort: { fields: publishedDate, order: DESC}) {
                 edges {
                     node {
-                        frontmatter {
-                            title
-                            date
-                        }
-                        fields {
-                            slug
-                        }
+                        title
+                        slug
+                        publishedDate(formatString: "MMMM Do, YYYY")
                     }
                 }
-            }
-        }
+            }  
+        }  
     `);
+
+    // Static way of pulling in the data from .md file
+    // const data = useStaticQuery(graphql`
+    //     query {
+    //         allMarkdownRemark {
+    //             edges {
+    //                 node {
+    //                     frontmatter {
+    //                         title
+    //                         date
+    //                     }
+    //                     fields {
+    //                         slug
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `);
 
     // console.log(data)
 
     return (
         <Layout>
-            <h1>Blog</h1>
-         <ol className={blogStyles.posts}>  
-                 {data.allMarkdownRemark.edges.map((e, i) => {
-                     return (
-                         <li className={blogStyles.post} key={i}>
-                             <Link to={`/blog/${e.node.fields.slug}`}>
-                                <h2>{e.node.frontmatter.title}</h2>
-                                <p>{e.node.frontmatter.date}</p>
-                             </Link>
-                         </li>
-                     )
-                 })}
-         </ol>
+        <h1>Blog</h1>
+        <ol className={blogStyles.posts}>  
+                {data.allContentfulBlogPost.edges.map((e, i) => {
+                    return (
+                        <li className={blogStyles.post} key={i}>
+                            <Link to={`/blog/${e.node.slug}`}>
+                                <h2>{e.node.title}</h2>
+                                <p>{e.node.publishedDate}</p>
+                            </Link>
+                        </li>
+                    )
+                })}
+        </ol>
         </Layout>
+
+
+        // Static Way
+        // <Layout>
+        //     <h1>Blog</h1>
+        //  <ol className={blogStyles.posts}>  
+        //          {data.allMarkdownRemark.edges.map((e, i) => {
+        //              return (
+        //                  <li className={blogStyles.post} key={i}>
+        //                      <Link to={`/blog/${e.node.fields.slug}`}>
+        //                         <h2>{e.node.frontmatter.title}</h2>
+        //                         <p>{e.node.frontmatter.date}</p>
+        //                      </Link>
+        //                  </li>
+        //              )
+        //          })}
+        //  </ol>
+        // </Layout>
     )
 }
 
