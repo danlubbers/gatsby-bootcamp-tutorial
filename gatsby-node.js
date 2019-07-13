e296f3c6772ceeefecbdf843e84cdf0ef6aa161f
Dan Lubbers <lubbers.dan@gmail.com>
2019-07-13 14:53:19 -0400

    Step 18: Dynamic Pages from Contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,24 @@
 const path = require('path');
 
 // onCreateNode creates the slug for the blog
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
-    // Transform the new node here and create a new node or
-    // create a new node field.
-    if(node.internal.type === 'MarkdownRemark') {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        // console.log(JSON.stringify(node , undefined, 4))
-        // console.log('@@@@@@@@@@@@@', slug)
+// THIS WAS ONLY BEING USED WITHOUT CONTENTFUL, now that we use CONTENTFUL we do NOT need this
+// module.exports.onCreateNode = ({ node, actions }) => {
+//     const { createNodeField } = actions
+//     // Transform the new node here and create a new node or
+//     // create a new node field.
+//     if(node.internal.type === 'MarkdownRemark') {
+//         const slug = path.basename(node.fileAbsolutePath, '.md')
+//         // console.log(JSON.stringify(node , undefined, 4))
+//         // console.log('@@@@@@@@@@@@@', slug)
 
-        createNodeField({
-            node, 
-            name: 'slug',
-            value: slug
-        })
+//         createNodeField({
+//             node, 
+//             name: 'slug',
+//             value: slug
+//         })
 
-    }
-}
+//     }
+// }
 
 // CreatePages dynamically creates the blog pages
 module.exports.createPages = async ({ graphql, actions }) => {
@@ -25,32 +26,57 @@ module.exports.createPages = async ({ graphql, actions }) => {
     
     // 1. Get Path to Template
     const blogTemplate = path.resolve('./src/templates/blog.js')
+    // New way to access the slug off the Contentful API
     const res = await graphql(`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost {
                 edges {
                     node {
-                        fields {
-                            slug
-                        }
+                      slug
                     }
                 }
             }
         }
     `)
+    // STATIC OLD WAY TO GENERATE PAGES WITHOUT CONTENTFUL
+    // const res = await graphql(`
+    //     query {
+    //         allMarkdownRemark {
+    //             edges {
+    //                 node {
+    //                     fields {
+    //                         slug
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `)
 
-    // 2. Get markdown data
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
-        // 3. Create new pages
-        // This will run twice creating 2 new pages /react and /gatsby
+    // CONTENTFUL WAY
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
     })
+
+    // STATIC OLD WAY
+    // 2. Get markdown data
+    // res.data.allMarkdownRemark.edges.forEach((edge) => {
+        // 3. Create new pages
+        // This will run twice creating 2 new pages /react and /gatsby
+    //     createPage({
+    //         component: blogTemplate,
+    //         path: `/blog/${edge.node.fields.slug}`,
+    //         context: {
+    //             slug: edge.node.fields.slug
+    //         }
+    //     })
+    // })
 
 
 }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,61 @@
 import React from 'react';
 import { graphql } from 'gatsby'; 
 import Layout from '../Components/Layout';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-// gatsby will grab this query and run it using export
+
+// gatsby will grab this query and run it using export for .md
+// export const query = graphql`
+//     query ($slug: String!) {
+//         markdownRemark (fields: { slug: { eq: $slug} }) {
+//           frontmatter {
+//             title
+//             date
+//           }
+//           html
+//         }
+//     }
+// `
+
 export const query = graphql`
     query ($slug: String!) {
-        markdownRemark (fields: { slug: { eq: $slug} }) {
-          frontmatter {
+        contentfulBlogPost (slug: {eq: $slug}) {
             title
-            date
-          }
-          html
+            publishedDate(formatString: "MMMM Do, YYYY")
+            body {
+                json
+            }
         }
     }
-`
+`;
 
 const Blog = (props) => {
+    // This allows us to get and generate images in the blog post body
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                // We got these from graphQL playground 
+                const alt = node.data.target.fields.title['en-US']
+                const url = node.data.target.fields.file['en-US'].url
+                return <img alt={alt} src={url}/>
+            }
+        }
+    }
+
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            {/* dangerouslySetInnerHTML is used to set a markup from an html string */}
-            <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
         </Layout>
+
+        // Static Old way 
+        // <Layout>
+        //     <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        //     <p>{props.data.markdownRemark.frontmatter.date}</p>
+        //     {/* dangerouslySetInnerHTML is used to set a markup from an html string */}
+        //     <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
+        // </Layout>
     )
 }
 
